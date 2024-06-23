92c5bc69c478e119505a1fbf26b4ca63cd54e204
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-06-23 05:57:17 +0200

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::io::stdout;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    stdout().flush().unwrap();
 
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    let result: Vec<&str> = input.split_whitespace().collect();
+
+    if let [cmd, args @ ..] = result.as_slice() {
+        match cmd {
+            _ => println!("{}: command not found", cmd),
+        }
+    }
 }
