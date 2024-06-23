054d86d19a9bcac14a5ed4575761fddc7bf7ad45
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-06-23 06:18:02 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use std::io::stdout;
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self};
+
+use anyhow::Ok;
 
 fn main() {
-    print!("$ ");
-    stdout().flush().unwrap();
+    let mut prompter = Prompter::new(io::stdin().lock(), io::stdout().lock());
 
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    loop {
+        prompter.write("$ ").unwrap();
 
-    let result: Vec<&str> = input.split_whitespace().collect();
-
-    if let [cmd] = result.as_slice() {
-        match cmd {
-            _ => println!("{}: command not found", cmd),
+        let input = prompter.read().unwrap();
+        let result: Vec<&str> = input.split_whitespace().collect();
+        if let [cmd] = result.as_slice() {
+            match cmd {
+                _ => println!("{}: command not found", cmd),
+            }
         }
+    }
+}
+
+struct Prompter<R: io::BufRead, W: io::Write> {
+    input: R,
+    output: W,
+}
+
+impl<R: io::BufRead, W: io::Write> Prompter<R, W> {
+    fn new(input: R, output: W) -> Self {
+        return Prompter { input, output };
+    }
+
+    fn read(&mut self) -> anyhow::Result<String> {
+        let mut input = String::new();
+        self.input.read_line(&mut input)?;
+
+        return Ok(input.trim().to_string());
+    }
+
+    fn write(&mut self, prompt: &str) -> anyhow::Result<()> {
+        write!(self.output, "{}", prompt)?;
+        self.output.flush()?;
+
+        return Ok(());
     }
 }
