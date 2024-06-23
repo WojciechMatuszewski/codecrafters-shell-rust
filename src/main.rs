598a8806769e4352e5aeeba69707246201d45c11
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-06-23 06:58:40 +0200

    impl

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,94 @@
-use std::io::{self};
+use std::{io, str::FromStr};
 
-use anyhow::Ok;
+use anyhow::anyhow;
 
-fn main() {
-    let mut prompter = Prompter::new(io::stdin().lock(), io::stdout().lock());
+fn main() -> anyhow::Result<()> {
+    let reader = io::stdin().lock();
+    let writer = io::stdout();
+    let mut prompter = Prompter::new(reader, writer);
 
     loop {
-        prompter.write("$ ").unwrap();
+        prompter.prompt("$ ")?;
 
-        let input = prompter.read().unwrap();
+        let input = prompter.read()?;
         let result: Vec<&str> = input.split_whitespace().collect();
-        if let [cmd] = result.as_slice() {
-            match cmd {
+
+        if let [cmd, args @ ..] = result.as_slice() {
+            match cmd.trim().parse::<Command>()? {
+                Command::Exit => {
+                    let code = args
+                        .get(0)
+                        .ok_or(anyhow!("Invalid arguments"))?
+                        .parse::<i32>()?;
+
+                    std::process::exit(code)
+                }
                 _ => println!("{}: command not found", cmd),
             }
         }
     }
 }
 
+enum Command {
+    Exit,
+    Unknown,
+}
+
+impl FromStr for Command {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Command> {
+        match s {
+            "exit" => Ok(Command::Exit),
+            _ => Ok(Command::Unknown),
+        }
+    }
+}
+
 struct Prompter<R: io::BufRead, W: io::Write> {
-    input: R,
-    output: W,
+    reader: R,
+    writer: W,
 }
 
 impl<R: io::BufRead, W: io::Write> Prompter<R, W> {
-    fn new(input: R, output: W) -> Self {
-        return Prompter { input, output };
+    fn new(reader: R, writer: W) -> Self {
+        return Prompter { reader, writer };
     }
 
     fn read(&mut self) -> anyhow::Result<String> {
         let mut input = String::new();
-        self.input.read_line(&mut input)?;
+        self.reader.read_line(&mut input)?;
 
         return Ok(input.trim().to_string());
     }
 
-    fn write(&mut self, prompt: &str) -> anyhow::Result<()> {
-        write!(self.output, "{}", prompt)?;
-        self.output.flush()?;
+    fn prompt(&mut self, prompt: &str) -> anyhow::Result<()> {
+        write!(self.writer, "{}", prompt)?;
+        self.writer.flush()?;
 
         return Ok(());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prompter() {
+        let input = b"Hi there";
+        let mut output = Vec::new();
+
+        let mut prompter = Prompter::new(input.as_slice(), &mut output);
+
+        prompter.prompt("first line\n").unwrap();
+        prompter.prompt("second line\n").unwrap();
+
+        let answer = prompter.read().unwrap();
+
+        let written = String::from_utf8(output).unwrap();
+
+        assert_eq!("first line\nsecond line\n", written);
+        assert_eq!("Hi there", answer);
     }
 }
