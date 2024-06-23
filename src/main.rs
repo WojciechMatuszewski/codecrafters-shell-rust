4949fc92074b6010a6eaa31f9e1011e392527cd4
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-06-23 07:14:13 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{io, str::FromStr};
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Ok};
 
 fn main() -> anyhow::Result<()> {
     let reader = io::stdin().lock();
@@ -11,37 +11,53 @@ fn main() -> anyhow::Result<()> {
         prompter.prompt("$ ")?;
 
         let input = prompter.read()?;
-        let result: Vec<&str> = input.split_whitespace().collect();
 
-        if let [cmd, args @ ..] = result.as_slice() {
-            match cmd.trim().parse::<Command>()? {
-                Command::Exit => {
-                    let code = args
-                        .get(0)
-                        .ok_or(anyhow!("Invalid arguments"))?
-                        .parse::<i32>()?;
-
-                    std::process::exit(code)
-                }
-                _ => println!("{}: command not found", cmd),
+        match input.parse::<Command>()? {
+            Command::Exit(code) => std::process::exit(code),
+            Command::Echo(prompt) => prompter.prompt(&prompt)?,
+            Command::Unknown(cmd) => {
+                let not_found = format!("{}: command not found\n", cmd);
+                prompter.prompt(&not_found)?;
             }
         }
     }
 }
 
 enum Command {
-    Exit,
-    Unknown,
+    Exit(i32),
+    Echo(String),
+    Unknown(String),
 }
 
 impl FromStr for Command {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> anyhow::Result<Command> {
-        match s {
-            "exit" => Ok(Command::Exit),
-            _ => Ok(Command::Unknown),
+        let parsed: Vec<&str> = s.split_whitespace().collect();
+
+        if let [cmd, args @ ..] = parsed.as_slice() {
+            let cmd = cmd.trim();
+
+            match cmd {
+                "exit" => {
+                    let code = args
+                        .get(0)
+                        .ok_or(anyhow!("Invalid arguments"))?
+                        .parse::<i32>()?;
+
+                    return Ok(Self::Exit(code));
+                }
+                "echo" => {
+                    let prompt = args.join(" ");
+                    let prompt = format!("{}\n", prompt);
+
+                    return Ok(Self::Echo(prompt));
+                }
+                _ => return Ok(Self::Unknown(cmd.to_string())),
+            }
         }
+
+        panic!("Could not parse the command")
     }
 }
 
